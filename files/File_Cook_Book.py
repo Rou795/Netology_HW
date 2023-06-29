bcfc56429d1229a5f88a6e66366b6817a528648b
Evgeniy Kukarskiy <jon.79555@gmail.com>
2023-06-29 11:47:39 +0700

    Add comments in Cook_book task

--- files/File_Cook_Book.py
+++ files/File_Cook_Book.py
@@ -1,6 +1,7 @@
 cook_book = {}
 receipts = []
 receipt = ''
+# Создание списка рецептов с разделением по блюдам
 with open('Cook_Book.txt') as f:
     for line in f.readlines():
         if line.isspace():
@@ -9,6 +10,7 @@ with open('Cook_Book.txt') as f:
         else:
             receipt += line
     receipts.append(receipt)
+# Создание требуемого словаря
 
 for receipt in receipts:
     receipt_list = receipt.split('\n')
@@ -21,15 +23,19 @@ for receipt in receipts:
             ingredient_dict['quantity'] = ingredient_list[1].strip()
             ingredient_dict['measure'] = ingredient_list[2].strip()
             cook_book[receipt_list[0]].append(ingredient_dict)
+# Реализация требуемой функции через адресацию по dish в словаре cook_book
+
 
 def get_shop_list_by_dishes(dishes, person_count):
     shop_dict = {}
     for dish in dishes:
         for el in cook_book[dish]:
             if not(el['ingredient_name'] in shop_dict.keys()):
-                shop_dict[el['ingredient_name']] = dict(measure = el['measure'], quantity = int(el['quantity']) * person_count)
+                shop_dict[el['ingredient_name']] = dict(measure=el['measure'], \
+                                                        quantity=int(el['quantity']) * person_count)
             else:
                 shop_dict[el['ingredient_name']]['quantity'] += int(el['quantity']) * person_count
     return shop_dict
+
 
 print(get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2))
